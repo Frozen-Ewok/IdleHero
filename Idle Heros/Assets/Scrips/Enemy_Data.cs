6e315226c2fabdfee6126831a43a43462276733a
Frozen-Ewok <tyler_mac_d@hotmail.com>
2015-07-01 13:53:23 +1200

    Added Project

---
+++ Idle Heros/Assets/Scrips/Enemy_Data.cs
@@ -0,0 +1,79 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Enemy_Data : MonoBehaviour {
+
+	private int m_iLevel;
+
+	public float m_fDamage;
+
+	public float m_fHealth;
+
+	public double m_fGold_Worth;
+	public double m_fExp_Worth;
+
+	private GameObject m_goHero;
+	private float m_fAttack_Timer = 0.0f;
+
+	private Hero_Data HeroScript;
+
+	// Use this for initialization
+	void Start () 
+	{
+		m_goHero = GameObject.FindGameObjectWithTag("Hero");
+
+		HeroScript = m_goHero.GetComponent<Hero_Data>();
+	}
+
+	public void SetBaseStats()
+	{
+		m_fGold_Worth = Random.Range(3,5) * 1.3; 
+		m_fHealth = 10 * (m_iLevel * 0.65f);
+		m_fExp_Worth = m_iLevel * 1.8f;
+		
+		m_fDamage = m_iLevel * 2.2f;
+	}
+	
+	// Update is called once per frame
+	void Update () 
+	{
+		if(m_fHealth <= 0)
+		{
+			HeroScript.m_dGold += m_fGold_Worth;
+			HeroScript.m_iCurrnet_Exp += m_fExp_Worth;
+
+			Destroy(gameObject);
+		}
+
+		AttackPlayer();
+	}
+
+	void AttackPlayer()
+	{
+		m_fAttack_Timer += Time.deltaTime;
+		Vector3 Direction = m_goHero.transform.position - transform.position;
+
+		//move in to attack distance
+		if(Direction.magnitude > 2)
+		{
+			Direction.Normalize();
+			transform.forward = Direction;
+			transform.position += transform.forward * Time.deltaTime * 10;
+		}
+		//now in range to attack
+		else
+		{
+			if(m_fAttack_Timer > 1)
+			{
+				m_fAttack_Timer = 0.0f;
+
+				HeroScript.m_fHealth -= m_fDamage;
+			}
+		}
+	}
+
+	public void SetEnemyLevel(int _iLevel)
+	{
+		m_iLevel = _iLevel;
+	}
+}
---
+++ Idle Heros/Assets/Scrips/Hero_Data.cs
@@ -0,0 +1,140 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Hero_Data : MonoBehaviour 
+{
+	//Hero Stats
+	public int m_iLevel = 1;
+	private int m_iMax_Level = 5000;
+
+	public int m_iStatPoints = 0;
+
+	private float m_fAttack_Range = 2.0f;
+
+	public double m_iExp_To_Level = 20;
+	public double m_iCurrnet_Exp = 0;
+
+	public float m_fDamage = 2.0f;
+
+	public float m_fMax_Health = 100f;
+	public float m_fHealth = 100f;
+	public float m_fHealth_Regen = 0.6f;
+
+	public float m_fMax_Mana = 50f;
+	public float m_fMana = 50f;
+	public float m_fMana_Regen = 0.1f;
+
+	public float m_fAttack_Speed = 1.0f;
+	private float m_fAttack_Timer = 0.0f;
+
+	public double m_dGold = 0;
+
+	private float m_fRegenTimer = 0;
+	private GameObject m_gEnemy;
+	private Enemy_Data m_EnemyScript;
+
+	// Use this for initialization
+	void Start () 
+	{
+	
+	}
+	
+	// Update is called once per frame
+	void Update () 
+	{
+		if(m_iCurrnet_Exp >= m_iExp_To_Level) LevelUp();
+
+		Regen();
+		Attack();
+	}
+
+	//Do all the level upping and change the m_iExp_To_Level
+	void LevelUp ()
+	{
+		if(m_iLevel <= m_iMax_Level)
+		{
+			double Exp_Overflow = m_iCurrnet_Exp - m_iExp_To_Level;
+			m_iLevel++;
+
+			m_iExp_To_Level = (m_iExp_To_Level + m_iLevel) * 1.10f; 
+
+			m_iStatPoints += 2;
+
+			m_iCurrnet_Exp = Exp_Overflow;
+		}
+	}
+
+	//Apply all the regen
+	void Regen()
+	{
+		m_fRegenTimer += Time.deltaTime;
+
+		if(m_fRegenTimer >= 1.0f)
+		{
+			m_fRegenTimer = 0;
+
+			if(m_fHealth + m_fHealth_Regen >= m_fMax_Health)
+			{
+				m_fHealth = m_fMax_Health;
+			}
+			else
+			{
+				m_fHealth += m_fHealth_Regen;
+			}
+
+			if(m_fMana + m_fMana_Regen >= m_fMax_Mana)
+			{
+				m_fMana = m_fMax_Mana;
+			}
+			else
+			{
+				m_fMana += m_fMana_Regen;
+			}
+		}
+	}
+	void Attack()
+	{
+		
+		m_fAttack_Timer += Time.deltaTime;
+		//if the enemy is vaild then attack it.
+		if(m_gEnemy)
+		{
+			Vector3 Direction = m_gEnemy.transform.position - transform.position;
+
+			//The enemy is in attack range
+			if(Direction.magnitude < m_fAttack_Range)
+			{
+				Direction.Normalize();
+				transform.forward = Direction;
+
+				if(m_fAttack_Timer > m_fAttack_Speed)
+				{
+					m_fAttack_Timer = 0.0f;
+
+					m_EnemyScript.m_fHealth -= m_fDamage;
+				}
+			}
+		}
+		else
+		{
+			float Nearest = 100000.0f;
+
+			GameObject[] NearestEnemy = GameObject.FindGameObjectsWithTag("Enemy");
+
+			for(int i = 0; i < NearestEnemy.Length; ++i)
+			{
+				Vector3 Direction = NearestEnemy[i].transform.position - transform.position;
+
+				if(Direction.magnitude < Nearest)
+				{
+					m_gEnemy = NearestEnemy[i];
+					Nearest = Direction.magnitude;
+				}
+			}
+			if(m_gEnemy)
+			{
+				m_EnemyScript = m_gEnemy.GetComponent<Enemy_Data>();
+			}
+		}
+	}
+}
